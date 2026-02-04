1380fbf937a0c3d77f707126023cc9197066f01d
Ravi Kant Gupta <kantgupta1995@gmail.com>
2026-02-04 17:51:19 +0530

    changed prisma schema to store refresh token id. now login api will send access and refresh token.

--- config/config.js
+++ config/config.js
@@ -9,10 +9,11 @@ const dbConfig = {
     db_port: process.env.DB_PORT,
     db_url: process.env.DATABASE_URL
   },
-
   app:{
     port: process.env.PORT
-  }
+  },
+  jwt: process.env.JWT_SECRET || 'your_jwt_secret_key',
+  bcrypt_salt_rounds: process.env.BCRYPT_SALT_ROUNDS || 10
   
 };
 
--- utils/app.constant.js
+++ utils/app.constant.js
@@ -1,6 +1,6 @@
 
 export const USER_ERROR_MESSAGES = Object.freeze({
-  NAME_OR_EMAIL_ALREADY_EXISTS: 'Name or email already exists',
+  EMAIL_ALREADY_EXISTS: 'Email already exists',
   USER_REGISTERED_SUCCESSFULLY: 'User registered successfully',
   USER_REGISTRATION_FAILED: 'Failed to register user',
   INVALID_PASSWORD: 'Invalid password',
@@ -11,5 +11,17 @@ export const USER_ERROR_MESSAGES = Object.freeze({
   TOKEN_REFRESHED: 'Token refreshed',
   INVALID_EMAIL: 'Invalid email',
   PASSWORD_RESET_FAILED: 'Failed to reset password',
-  PASSWORD_RESET_SUCCESSFUL: 'Password reset successful'
+  PASSWORD_RESET_SUCCESSFUL: 'Password reset successful',
+  LOGIN_FAILED: 'Login failed'
 });
+
+export const AUTH_ERROR_MESSAGES = Object.freeze({
+  REFRESH_TOKEN_EXPIRED: 'Refresh token has expired',
+  INVALID_REFRESH_TOKEN: 'Invalid refresh token'
+});
+
+export const SUCCESS_MESSAGE = Object.freeze({
+  SUCCESS : "success",
+  TOKEN_REFRESHED: 'Token refreshed',
+  PASSWORD_RESET_SUCCESSFUL: 'Password reset successful'
+})
